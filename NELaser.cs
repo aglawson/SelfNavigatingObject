8631de80e1a6a53ae02724df13027954c4c7c156
aglawson <36459896+aglawson@users.noreply.github.com>
2020-01-21 15:08:47 -0800

    Scripts
    FollowPlayer: a script that makes an object (lasers, main camera) follow the main "player" or object.
    Laser Scripts: all laser scripts are essentially the same: they are rotated in different degrees to detect obstacles. Once a laser "collides" with an obstacle, a force is applied to the main object to push it away from that obstacle.
    PlayerCollision: this script disables main object movement when it collides with an obstacle
    PlayerMovement: contains the instructions for allowing user to move main object

---
+++ NELaser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NELaser : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    public Rigidbody rb;
+    public float upForce = 100;
+    public float backForce = 100;
+    public float rightForce = 100;
+    public float leftForce = -100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+                rb.AddForce(0, 0, backForce);
+                rb.AddForce(0, upForce * Time.deltaTime, 0);
+                rb.AddForce(rightForce, 0, 0);
+                rb.AddForce(leftForce, 0, 0);
+            }
+        }
+        else lr.SetPosition(1, transform.forward);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000;
+    public float rightForce = 500;
+    public float leftForce = -500;
+    public float upForce = 500;
+    public float downForce = -500;
+    public float constantUp = 250;
+    
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //add a forward force
+        rb.AddForce(0, 0, forwardForce);
+        rb.AddForce(0, constantUp, 0);
+        
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(rightForce, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(leftForce, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, upForce, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, downForce, 0);
+        }
+    }
+
+
+
+}
